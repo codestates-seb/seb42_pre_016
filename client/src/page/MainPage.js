580bae3050d0a704c7997a18b24302e60003d75e
Leeseungkeun77 <116235181+Leeseungkeun77@users.noreply.github.com>
2023-02-21 14:16:08 +0900

    feat: questin 기본 스타일 구현 및 dummydata생성

--- client/src/page/MainPage.js
+++ client/src/page/MainPage.js
@@ -1,15 +1,10 @@
 import React from "react";
-import Head from "../components/Header/Head";
-import HeadLogout from "../components/Header/HeadLogout";
+import QuestionList from "../components/Questions/QuestionList";
+
 
 const Mainpage = () => {
   return (
-    <div>
-      <div>로그인 후</div>
-      <Head />
-      <div>로그인 전</div>
-      <HeadLogout />
-    </div>
+    <QuestionList/>
   );
 };
 
