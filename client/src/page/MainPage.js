5f15ab2f62a79275ae38aacc3dd1ccad52ce3356
minjusong <mindyumandyu@gmail.com>
2023-02-22 04:30:14 +0900

    feat : ask questions 페이지 화면 구현

--- client/src/page/MainPage.js
+++ client/src/page/MainPage.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Head from "../components/Header/Head";
 import Sidebar from "../components/Sidebar/Sidebar";
 import QuestionList from "../components/Questions/QuestionList";
+import AskQuestions from "../components/AskQuestions/AskQuestions";
 
 const WrapperBody = styled.div`
   margin: 0 auto;
