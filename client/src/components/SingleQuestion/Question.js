f85ab0bd57609759db23809903127c1e3e3fd257
haizellatte <haizellatte@naver.com>
2023-02-24 00:41:11 +0900

    feat : 질문페이지 답변 컴포넌트 생성

--- client/src/components/SingleQuestion/Question.js
+++ client/src/components/SingleQuestion/Question.js
@@ -2,12 +2,15 @@ import React from "react";
 import Qheader from "./Qheader";
 import Qbody from "./Qbody";
 import Qanswer from "./Qanswer";
+import Answers from "./Answers";
 
 const SingleQuestions = () => {
   return (
     <>
       <Qheader />
       <Qbody />
+      <Answers />
+      <Answers />
       <Qanswer />
     </>
   );
