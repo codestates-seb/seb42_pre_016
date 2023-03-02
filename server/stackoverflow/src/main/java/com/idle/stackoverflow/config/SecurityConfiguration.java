f0d5deb2ceb0d95829171788f21af3cd3258fc3d
jiyoonchol <jiyoon276479@gmail.com>
2023-03-03 00:44:11 +0900

    chore : CORS OPTIONS 추가

--- server/stackoverflow/src/main/java/com/idle/stackoverflow/config/SecurityConfiguration.java
+++ server/stackoverflow/src/main/java/com/idle/stackoverflow/config/SecurityConfiguration.java
@@ -74,7 +74,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("OPTIONS", "GET", "POST", "PATCH", "DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
